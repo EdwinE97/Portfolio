ea41e3d5d0822f381bbbe2288594e5d83cdd7df0
EdwinE97 <94918451+EdwinE97@users.noreply.github.com>
2022-04-22 11:54:39 -0400

    about

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () =>{
             <img className='img-me' alt='profile' src='./img/edwin pic.jpeg'></img>
             <h1 style={{color:'brown'}}>About Me</h1>
             <h2>Software Engineer</h2>
-            <p> Born in Ecuador, raised in NC. Throughout my career I have been a firm beli <a href='img/Edwin_E_Resume.pdf'><p style={{color:'brown'}}>here!</p></a> </p>
+            <p> Born in Ecuador, raised in NC. Curiosity has led me to be many different things in my life. From being a sales professional to becoming a meat cutter. Currently I am a coding apprentice at Road To Hire learning key concepts in both coding and professional development. Im excited to say that I am pursuing a career that may never fulfil my curiosity! Learning new coding languages and techniques to help solve pressing issues is what motivates me everyday. You can learn more about my work history   <a href='img/Edwin_E_Resume.pdf'><p style={{color:'brown'}}>here!</p></a> </p>
             
             </div>
         </div>
